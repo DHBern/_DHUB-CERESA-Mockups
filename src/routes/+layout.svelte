fe24c54a0c1fea72ddc6d641197457c5fdea6678
vvvyyynet <levyn.buerki@unibe.ch>
2025-10-31 18:38:48 +0100

    feat: split documents in routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,36 @@
 <script lang="ts">
+	import { base } from '$app/paths';
+
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+	let fassung = $state('LF');
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<nav class="flex flex-wrap gap-10 font-bold">
+	<a href={`${base}/eloisa1`}>Eloisa frammento 1</a>
+	<a href={`${base}/eloisa2`}>Eloisa frammento 2</a>
+	<a href={`${base}/poesie`}>Poesie</a>
+	<a href={`${base}/adamo`}>Adamo</a>
+	<a href={`${base}/anima`}>Anima</a>
+	<a href={`${base}/aborto`}>Aborto</a>
+	<a href={`${base}/carson`}>Carson McCullers</a>
+</nav>
+
+<button
+	onclick={() => {
+		fassung = fassung == 'DF' ? 'LF' : 'DF';
+	}}
+	class={['fixed top-10 left-0 rounded-xl p-2 px-5', fassung ? 'bg-red-200 font-bold' : '']}
+	>{fassung == 'DF' ? 'change to LF' : 'change to DF'}</button
+>
+
+<div data-fassung={fassung} class="m-20">
+	{@render children()}
+</div>
---
+++ src/routes/eloisa2/+page.svelte
@@ -0,0 +1,59 @@
+<h1 class="h1">Eloisa (frammento 2)</h1>
+<p>
+	<handFree>
+		Eloisa<br />
+		o della predestinazione<br />
+	</handFree>
+</p>
+<p>
+	<strike>
+		Le stranezze di Eloisa incominciarono a manifestarsi in giovanissima<br />
+		età, sebbene in modo curioso.<br />
+		È forse il caso di sapere subito che
+	</strike> <strike>n</strike><hand>N</hand>ella <strike>sua</strike> famiglia materna
+	<insOver>di Eloisa</insOver><br />
+	vigeva da tempo immemorabile l’usanza di battezzare una figlia con<br />
+	il nome della madre, per cui le Eloise vi si susseguivano <strike>con regolarità</strike><insSup
+		>senza posa</insSup
+	><br />
+	salvo forse rarissimi casi di assenza di prole<insOver>,</insOver> o comunque prole fem<hyphen
+		>-</hyphen
+	><br />
+	minile<insOver>,</insOver> per una o l’altra di esse,<hand>;</hand> ma si raccontava che si era
+	allora<br />
+	provvedut<strike>o</strike>a ristabilire l’ordine con un piccolo scarto laterale
+	<insOver>,</insOver><br />
+	incaricando della bisogna una madre acquisita per <strike>intermissione</strike><insSup
+		>tramite</insSup
+	>
+	maschile<insOver>,</insOver><br />
+	<strike>pur sempre riferibile alla</strike><insSup>salvaguardando la</insSup> discendenza diretta,
+	per cui questi lievi<br />
+	disordini non avevano mai se non minimissimamente inquietato la tradi<hyphen>-</hyphen><br
+	/>zione, perseguita con <strike>rara</strike><insSup>lodevole</insSup> pertinacia. Nella
+	contingenza dell’attuale<br />
+	Eloisa, in casa erano presenti ben tre Eloise lei compresa, giusta<hyphen>-</hyphen><br />mente
+	situate una alle spalle dell’altra in quanto una madre e l’altra<br />
+	nonna della nostra. La cosa non sembrava disturbare nessuno salvo<br />
+	forse <strike>in</strike><insSup>nei</insSup> rari momenti in cui a un richiamo accorrevano ben
+	tre esempla<hyphen>-</hyphen><br />ri; ma essendo due di essi provvisti anche di appellativi
+	strettamente<br />
+	funzionali e assai più usati, non si pensò di ovviare magari con<br />
+	un nomignolo a un inconveniente che a nessuno sembrava tale.<br data-type="lf" />
+	Se non, come ben tosto apparve, all’ultima arrivata.<br data-type="lf" />
+	Rimangono celebri i tentativi intrapresi dalla nostra per uscire<br />
+	dal vicolo cieco di un’omonimia evidentemente per lei incomprensibile<br />
+	e perseguiti con testarda determinazione fin dal momento in cui incomin<hyphen>-</hyphen><br />ciò
+	a dominare l’uso del linguaggio. Si ricordano le <strike>imperscrutabili</strike>
+	<insSup>preoccupanti</insSup><br />
+	ottusità opposte dalla medesima alle spiegazioni circa geografia<br />
+	e geometria dei legami parentali, per cui risultò impossibile tranquil<hyphen>-</hyphen><br
+	/>lizzare la piccola con la narrazione dell’usanza trasmessa di madre<br />
+	in figlia<insOver>,</insOver> o peggio ancora<insOver>,</insOver> nel nostro caso, di madre in
+	madre: dove<br />
+	appare dopotutto non troppo peregrina la manifesta difficoltà di<br />
+	figlia e nipote a vederne la logica concatenazione, <hand>;</hand>se non poi eviden<hyphen
+		>-</hyphen
+	><br />temente con l’andare del tempo. Seguirono <insSup>le</insSup> intemperanze di proteste<br
+	/>
+</p>
