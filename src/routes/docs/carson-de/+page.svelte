22460f38a5501f52624fb61b0b90b3d3e07ab094
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-01 18:59:19 +0100

    fix: replace old carson

--- src/routes/docs/carson-de/+page.svelte
+++ src/routes/docs/carson-de/+page.svelte
@@ -7,102 +7,225 @@
 		<img src={`${base}/images/carson.jpeg`} alt="Scan" />
 	</div>
 	<div class="p-15">
-		<h1 class="h1">
-			<a data-person href="https://explore.gnd.network/gnd/118575724" target="_blank"
-				>Carson McCullers</a
-			>
-		</h1>
+		<h1 class="h1">Carson McCullers</h1>
 		<p>
-			<span class="font-bold">Carson McCuller<insEditorial>s</insEditorial></span><br break="no" />
+			<br /><a type="person" href="https://explore.gnd.network/gnd/118575724" target="_blank"
+				><strong>Carson McCullers</strong></a
+			>
+			<br break="no" />
 			Eine junge amerikanische Schriftstellerin setzt sich durch.<br break="no" />
 			<br break="no" />
 		</p>
 		<p>
-			<a data-person href="https://explore.gnd.network/gnd/118575724" target="_blank"
+			<a type="person" href="https://explore.gnd.network/gnd/118575724" target="_blank"
 				>Carson McCullers</a
 			>
-			ist heute bestimmt die meistgelesene und meistübersetzte junge amerikanische Schriftstellerin.
-			Im Staate
-			<a data-place href="https://explore.gnd.network/gnd/4020273-2" target="_blank">Georgia</a> im
-			Jahre 1917 geboren, schrieb sie zwischen zwanzig und siebenundzwanzig Jahren drei
-			aufsehenerregende Romane:
-			<a data-work href="https://explore.gnd.network/gnd/1217441484" target="_blank"
-				>«The Heart is a lonely Hunter»</a
-			>,
-			<a data-work href="https://explore.gnd.network/gnd/1143730216" target="_blank"
-				>«Reflections in a golden Eye»</a
-			>
-			und
-			<a data-work href="https://explore.gnd.network/gnd/4313598-5" target="_blank"
-				>«The Member of the Wedding»</a
-			>. Alle drei erschienen in der französischen Uebersetzung bei
-			<a data-publisher href="" target="_blank">Stock</a>
-			– und der letzte, unter dem Titel <a data-work href="" target="_blank">«Frankie Addams»</a>,
-			erst kürzlich – und fanden, man kann den Ausdruck wahrheitsgetreu benutzen, in Frankreich eine
-			begeisterte Aufnahme. Dies, trotzdem die junge Autorin sich kaum zu den in Frankreich so
-			beliebten Auseinandersetzungen über die Problematik amerikanischer und europäischer
-			Roman-Literatur (sprich «recherches romanesques») verwerten lässt. Wir wissen, biographisch
-			gesprochen, nicht viel von
-			<a data-person href="https://explore.gnd.network/gnd/118575724" target="_blank"
+			ist heute bestimmt <br />die meistgelesene und meistübersetzte
+			<br />junge amerikanische Schriftstellerin. Im <br />Staate
+			<a type="place" href="https://explore.gnd.network/gnd/4020273-2" target="_blank">Georgia</a>
+			im Jahre 1917 geboren, <br />schrieb sie zwischen zwanzig und sieben<hyphen>-</hyphen><br
+			/>undzwanzig Jahren drei aufsehenerregen<hyphen>-</hyphen><br />de Romane:
+			<choice>
+				<orig>«</orig>
+				<reg />
+			</choice><a type="work" href="https://explore.gnd.network/gnd/1217441484" target="_blank"
+				>The Heart is a lonely Hun<hyphen>-</hyphen><br />ter</a
+			><choice>
+				<orig>»</orig>
+				<reg />
+			</choice>,
+			<choice>
+				<orig>«</orig>
+				<reg />
+			</choice><a type="work" href="https://explore.gnd.network/gnd/1143730216" target="_blank"
+				>Reflections in a golden Eye</a
+			><choice>
+				<orig>»</orig>
+				<reg />
+			</choice>
+			und <br /><choice>
+				<orig>«</orig>
+				<reg />
+			</choice><a type="work" href="https://explore.gnd.network/gnd/4313598-5" target="_blank"
+				>The Member of the Wedding</a
+			><choice>
+				<orig>»</orig>
+				<reg />
+			</choice>. Alle drei <br />erschienen in der französischen Ueber<hyphen>-</hyphen><br
+			/>setzung bei
+			<a type="org" key="#org_9900">Stock</a>
+			– und der letzte, unter <br />dem Titel
+			<choice>
+				<orig>«</orig>
+				<reg />
+			</choice><a type="work" key="#work_9903">Frankie Addams</a><choice>
+				<orig>»</orig>
+				<reg />
+			</choice>, erst kürz<hyphen>-</hyphen><br />lich – und fanden, man kann den Aus<hyphen
+				>-</hyphen
+			><br />druck wahrheitsgetreu benutzen, in
+			<br /><a type="place" key="#place_9901">Frankreich</a> eine begeisterte Aufnahme.
+			<br />Dies, trotzdem die junge Autorin sich
+			<br />kaum zu den in <a type="place" key="#place_9901">Frankreich</a> so beliebten <!-- Auseinandersetzungen … romanesques is commented as "annot. sem. romanzo (tema)"; question: should this refer to a keyword? if yes, not all keywords will be referencable in the source text; perahps allow keyword references only from editorial notes or document-level metadata -->
+			<br />Auseinandersetzungen über die Problema<hyphen>-</hyphen><br />tik amerikanischer und
+			europäischer Ro<hyphen>-</hyphen><br />man-Literatur (sprich «<foreign xml:lang="fr"
+				>recherches roma<hyphen>-</hyphen><br />nesques</foreign
+			>») verwerten lässt. Wir wissen,
+			<br />biographisch gesprochen, nicht viel von <br /><a
+				type="person"
+				href="https://explore.gnd.network/gnd/118575724"
+				target="_blank">Carson McCullers</a
+			>; rare Photographien <br />zeigen uns das blasse, sehr tragische und
+			<br />anderseits fast kindliche Gesicht dieser <br />überaus begabten Schriftstellerin, die be<hyphen
+				>-</hyphen
+			><br />reits ihr erstes Buch in meisterhafter Be<hyphen>-</hyphen><br />herrschung von Stil
+			und Ausdruck <br />schrieb, und von deren Büchern man sagt, <br />sie seien grausam und eines
+			<a type="person" key="#pers_9001">Faulkners</a>
+			<br />würdig. Ohne sich in eine solche etwas <br />simplistische «Abfertigung» der Sonder<hyphen
+				>-</hyphen
+			><br />lichkeit
+			<a type="person" href="https://explore.gnd.network/gnd/118575724" target="_blank"
 				>Carson McCullers</a
-			>; rare Photographien zeigen uns das blasse, sehr tragische und anderseits fast kindliche
-			Gesicht dieser überaus begabten Schriftstellerin, die bereits ihr erstes Buch in meisterhafter
-			Beherrschung von Stil und Ausdruck schrieb, und von deren Büchern man sagt, sie seien grausam
-			und eines Faulkners würdig. Ohne sich in eine solche etwas simplistische «Abfertigung» der
-			Sonderlichkeit
-			<a data-person href="https://explore.gnd.network/gnd/118575724" target="_blank"
-				>Carson McCullers</a
 			>
-			zu verirren, kann man doch nicht umhin, eine gewisse Wahrheit darin zu erkennen. Tatsächlich zeichnen
-			sich alle drei oben erwähnten Romane durch ihren erbarmungslos objektiven und kalt aufgebauten
-			Gang aus; und vielleicht gibt er ihnen jenen Anschein von Grausamkeit – denn umsonst würde man
-			sie, die Grausamkeit, in der Auswahl und der Behandlung des «Inhalts» suchen. Cocteau sagte einmal:
-			«Der Stil, ist die Gangart des Geschriebenen»; demnach also wäre zu sagen,
-			<a data-person href="https://explore.gnd.network/gnd/118575724" target="_blank"
-				>Carson McCullers</a
+			zu verirren, <br />kann man doch nicht umhin, eine gewisse
+			<br />Wahrheit darin zu erkennen. Tatsächlich <br />zeichnen sich alle drei oben erwähnten
+			<br />Romane durch ihren erbarmungslos ob<hyphen>-</hyphen><br />jektiven und kalt aufgebauten
+			Gang aus;
+			<br />und vielleicht gibt er ihnen jenen An<hyphen>-</hyphen><br />schein von Grausamkeit –
+			denn umsonst <br />würde man sie, die Grausamkeit, in der <br />Auswahl und der Behandlung des
+			«In<hyphen>-</hyphen><br /><br />halts» suchen. <a type="person" key="#pers_9902">Cocteau</a>
+			sagte einmal: «Der
+			<br />Stil, ist die Gangart des Geschriebenen»; <br />demnach also wäre zu sagen,
+			<a type="person" href="https://explore.gnd.network/gnd/118575724" target="_blank"
+				>Carson <choice>
+					<orig>Mc <br />Cullers</orig>
+					<reg>Mc<hyphen>-</hyphen><br />Cullers</reg>
+				</choice></a
 			>
-			Stil sei grausam. Und somit wäre das Thema der Grausamkeit erschöpft, wie auch das Thema «Faulkner»
-			in bezug auf McCullers Werke. Behandeln sie doch alle drei, die Romane, ein einziges und stets
-			anders bearbeitetes aber wiederzuerkennendes Thema: das Irren, das unruhige, einsame Drängen des
-			menschlichen Herzens. So wie es der Titel des Erstlingswerkes auf eine erschütternde Art sagt:
-			«Das Herz ist ein einsamer Jäger.» So folgen die Romanfiguren jenen unterirdischen Pfaden, und
-			ihre Worte, und ihre Taten alle sind nur der im täglichen Leben davon sichtbare Widerschein. Mick,
-			das junge noch nicht ausgewachsene Mädchen, schleicht sich in den langen Sommerabenden jenen Häusern
-			entlang, aus deren Fenster es Musik ertönen hört, auf der Suche nach einer Melodie, in der es sich
-			selbst finden könnte und die das Namenlose ausdrückt, das es bedrängt. Der blasse, taubstumme Bursche,
-			der eigentlich im Mittelpunkt des Buches steht, vergibt sich vollkommen in den Erklärungen, Bewegungen
-			seiner Hände, die er immer und immer wieder, ohne je erfahren zu können, ob der andere ihn versteht,
-			vor den abwesenden Augen seines ebenfalls taubstummen Freundes beginnt. Und alle anderen, die vielen
-			bunten Figuren des Buches, stehen um ihn herum und sprechen zu ihm, der nicht Antwort geben kann,
-			von dem was ihr Herz und ihre Einsamkeit erfüllt. Es ist an[?] sich erschütternd zu denken, dass
-			ein zweiundzwanzigjähriges Mädchen diese von menschlichem Elend und tiefster Einsamkeit lebende
-			Geschichte erfinden konnte. Demnach schiene die junge amerikanische Literatur anderen, uns allen
-			näheren Problemen zu entspringen, als die der ihr vorhergehenden Generation.<br
-				data-type="lf"
-			/>
-			«Reflections in a Golden Eye» scheint ein viel mehr «literarisches» Werk zu sein; die Geschichte
-			ist auf eine absichtlich künstliche Art erzählt. Die Personen, in einer wahrhaftig «psychoanal[y]tischen
-			Beleuchtung» als reine Komparsen behandelt, werden mit fester Hand zu dem an sich banalen Ergebnis
-			eines Mordfalles geführt. Trotzdem enthält und umfasst gerade dieses «ausgeklügelte» Buch in seinen
-			wichtigsten Zügen die ganze Macht von
-			<a data-person href="https://explore.gnd.network/gnd/118575724" target="_blank"
-				>Carson McCullers</a
+			Stil sei grausam. Und somit wäre <br />das Thema der Grausamkeit erschöpft, <br />wie auch das
+			Thema «<a type="person" key="#person_9901">Faulkner</a>» in bezug
+			<br />auf
+			<a type="person" href="https://explore.gnd.network/gnd/118575724" target="_blank">McCullers</a
 			>
-			Erzählungskunst. Zusammenfassend und aufschlussgebend nicht nur über die Mittel und Zwecke seiner
-			Autorin, sondern auch und hauptsächlich über eine Zeit und ihre wichtigsten moralischen und psychologischen
-			Gegebenheiten. Von «The Member of the Wedding» wurde kürzlich in Manhattan eine von der Autorin
-			selbst verfasste Bühnenbearbeitung gegeben. Es geht hier um die weltfremde, törichte, traurige
-			Backfischjahrezeit. Mit welcher Sorgfalt, mit welcher Liebe zeichnet Carson M[cC]ullers die Welt
-			und das Wesen der Frankie Jasmine Addams, dieses zu hochgewachsenen zwölfjährigen Mädchens, dessen
-			Sehnsüchte um so inniger sind, als sie zwecklos, sinnlos in Phantasiegebilden und Unmöglichem herumirren.<br
-				break="no"
-			/>
-			Tatsächlich, wenn Carson M[cC]ullers, ihre Bücher und ihre Art, Schreiben und Leben aufzufassen,
-			das «junge Amerika» vertreten, dann berührt uns dieses junge Amerika eindringlicher, als das von
-			gestern.<br data-type="lf" />
-			Und den Schweizer Leser mag es berühren, dass die Originalfassung des Bandes «Reflections in a
-			golden Eye» die Widmung trägt: «To Annemarie Clarac-Schwarzenbach.» <br break="no" />
+			Werke. Behandeln sie <br />doch alle drei, die Romane, ein ein<hyphen>-</hyphen><br />ziges
+			und stets anders bearbeitetes aber
+			<br />wiederzuerkennendes Thema: das Irren, <br />das unruhige, einsame Drängen des
+			<br />menschlichen Herzens. So wie es der Titel <br />des Erstlingswerkes auf eine erschüttern<hyphen
+				>-</hyphen
+			><br />de Art sagt:
+			<choice>
+				<orig>«</orig>
+				<reg />
+			</choice><a type="work" href="https://explore.gnd.network/gnd/1217441484" target="_blank"
+				>Das Herz ist ein einsamer <br />Jäger.</a
+			><choice>
+				<orig>»</orig>
+				<reg />
+			</choice>
+			So folgen die Romanfiguren jenen <br />unterirdischen Pfaden, und ihre Worte, <br />und ihre
+			Taten alle sind nur der im täg<hyphen>-</hyphen><br />lichen Leben davon sichtbare Wider<hyphen
+				>-</hyphen
+			><br />schein. Mick, das junge noch nicht aus<hyphen>-</hyphen><br />gewachsene Mädchen,
+			schleicht sich in
+			<br />den langen Sommerabenden jenen Häu<hyphen>-</hyphen><br />sern entlang, aus deren
+			Fenster es Musik <br />ertönen hört, auf der Suche nach einer <br />Melodie, in der es sich
+			selbst finden könn<hyphen>-</hyphen><br />te und die das Namenlose ausdrückt, das <br />es
+			bedrängt. Der blasse, taubstumme Bur<hyphen>-</hyphen><br />sche, der eigentlich im
+			Mittelpunkt des <br />Buches steht, vergibt sich vollkommen in <br />den Erklärungen,
+			Bewegungen seiner <br />Hände, die er immer und immer wieder, <br />ohne je erfahren zu
+			können, ob der an<hyphen>-</hyphen><br />dere ihn versteht, vor den abwesenden
+			<br />Augen seines ebenfalls taubstummen <br />Freundes beginnt. Und alle anderen, die
+			<br />vielen bunten Figuren des Buches, stehen <br />um ihn herum und sprechen zu ihm, der
+			<br />nicht Antwort geben kann, von dem was <br />ihr Herz und ihre Einsamkeit erfüllt. Es
+			<br />ist
+			<unclear>an</unclear><!-- TODO: compare with NB copy --> sich erschütternd zu denken, dass
+			<br />ein zweiundzwanzigjähriges Mädchen <br />diese von menschlichem Elend und tief<hyphen
+				>-</hyphen
+			><br />ster Einsamkeit lebende Geschichte er<hyphen>-</hyphen><br />finden konnte. Demnach
+			schiene die jun<hyphen>-</hyphen><br /><br />ge amerikanische Literatur anderen, uns
+			<br />allen näheren Problemen zu entspringen, <br />als die der ihr vorhergehenden Genera<hyphen
+				>-</hyphen
+			><br />tion.
 		</p>
-		<p>Ceresa</p>
+		<p>
+			<choice>
+				<orig>«</orig>
+				<reg />
+			</choice><a type="work" href="https://explore.gnd.network/gnd/1143730216" target="_blank"
+				>Reflections in a golden Eye</a
+			><choice>
+				<orig>»</orig>
+				<reg />
+			</choice>
+			scheint <br />ein viel mehr «literarisches» Werk zu
+			<br />sein; die Geschichte ist auf eine absicht<hyphen>-</hyphen><br />lich künstliche Art
+			erzählt. Die Personen, <br />in einer wahrhaftig
+			<choice>
+				<sic>«psychoanalitischen <br />Beleuchtung»</sic>
+				<reg>«psychoanalytischen <br />Beleuchtung»</reg>
+			</choice>
+			als reine Komparsen be<hyphen>-</hyphen><br />handelt, werden mit fester Hand zu dem <br />an
+			sich banalen Ergebnis eines Mordfalles <br />geführt. Trotzdem enthält und umfasst
+			<br />gerade dieses «ausgeklügelte» Buch in <br />seinen wichtigsten Zügen die ganze Macht
+			<br />von
+			<a type="person" href="https://explore.gnd.network/gnd/118575724" target="_blank"
+				>Carson McCullers</a
+			>
+			Erzählungskunst. <br />Zusammenfassend und aufschlussgebend <br />nicht nur über die Mittel
+			und Zwecke sei<hyphen>-</hyphen><br />ner Autorin, sondern auch und haupt<hyphen>-</hyphen><br
+			/>sächlich über eine Zeit und ihre wichtig<hyphen>-</hyphen><br />sten moralischen und
+			psychologischen
+			<br />Gegebenheiten. Von
+			<choice>
+				<orig>«</orig>
+				<reg />
+			</choice><a type="work" href="https://explore.gnd.network/gnd/4313598-5" target="_blank"
+				>The Member of the
+				<br />Wedding</a
+			><choice>
+				<orig>»</orig>
+				<reg />
+			</choice>
+			wurde kürzlich in <a type="place" key="#place_9902">Manhattan</a>
+			<br />eine von der Autorin selbst verfasste <br />Bühnenbearbeitung gegeben. Es geht hier
+			<br />um die weltfremde, törichte, traurige
+			<br />Backfischjahrezeit. Mit welcher Sorgfalt, <br />mit welcher Liebe zeichnet Carson
+			<choice>
+				<orig>Mc <br />Cullers</orig>
+				<reg>Mc<hyphen>-</hyphen><br />Cullers</reg>
+			</choice>
+			die Welt und das Wesen der <br />Frankie Jasmine Addams, dieses zu hoch<hyphen>-</hyphen><br
+			/>gewachsenen zwölfjährigen Mädchens,
+			<br />dessen Sehnsüchte um so inniger sind, <br />als sie zwecklos, sinnlos in Phantasiege<hyphen
+				>-</hyphen
+			><br />bilden und Unmöglichem herumirren.
+		</p>
+		<p>
+			Tatsächlich, wenn Carson <choice>
+				<orig>Mc Cullers</orig>
+				<reg>McCullers</reg>
+			</choice>, <br />ihre Bücher und ihre Art, Schreiben und
+			<br />Leben aufzufassen, das «junge <a type="place" key="#place_9903">Amerika</a>»
+			<br />vertreten, dann berührt uns dieses junge <br /><a type="place" key="#place_9903"
+				>Amerika</a
+			>
+			eindringlicher, als das von ge<hyphen>-</hyphen><br />stern.
+		</p>
+		<p>
+			Und den Schweizer Leser mag es be<hyphen>-</hyphen><br />rühren, dass die Originalfassung des
+			<br />Bandes
+			<choice>
+				<orig>«</orig>
+				<reg />
+			</choice><a type="work" href="https://explore.gnd.network/gnd/1143730216" target="_blank"
+				>Reflections in a golden Eye</a
+			><choice>
+				<orig>»</orig>
+				<reg />
+			</choice>
+			die <br />Widmung trägt: «To
+			<a type="person" key="#pers_9903">Annemarie Clarac-<br />Schwarzenbach</a>.»
+		</p>
+		<byline><a type="person" key="#pers_9904">Ceresa</a></byline>
 	</div>
 </div>
