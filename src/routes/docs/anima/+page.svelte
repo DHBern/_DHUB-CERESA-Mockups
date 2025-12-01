edabd15051b7cd4fdface801818da70ea28a70d6
vvvyyynet <levyn.buerki@unibe.ch>
2025-12-01 20:05:40 +0100

    corr: anima

--- src/routes/docs/anima/+page.svelte
+++ src/routes/docs/anima/+page.svelte
@@ -18,12 +18,13 @@
 			<a data-crossref href={`${base}/docs/vita`}>VITA</a> risulta essere la forza attiva propria
 			degli esseri<br />
 			animali e vegetali in virtù della quale, oltre che muoversi,<br />
-			nutrirsi, assimilare e quant’altro, sono anche il grado di<br />
+			nutrirsi, assimilare e quant’altro, sono anche i<choice><sic>l</sic><corr>n</corr></choice>
+			grado di<br />
 			riprodursi sotto forme sempre uguali.
 		</p>
 		<p>
-			Fra anima e vita corre dunque un rapporto di reciproca dipend<hyphen>-</hyphen><br />denza il
-			quale tuttavia non risulta indispensabile alla vita<br />
+			Fra anima e vita corre dunque un rapporto di reciproca dipen<delete type="hand">d</delete
+			><hyphen>-</hyphen><br />denza il quale tuttavia non risulta indispensabile alla vita<br />
 			né animale né vegetale né altra, in quanto l’<a data-crossref href={`#`}>ANIMA</a> è alloggia<hyphen
 				>-</hyphen
 			><br />ta esclusivamente nella specie umana, sottospecie della fa<hyphen>-</hyphen><br
@@ -38,11 +39,11 @@
 			vermi, amebe o piante. Se ne deduce pertanto che l’<a data-crossref href={`#`}>ANIMA</a><br />
 			è un organismo non so<insert place="inline">l</insert>tanto invisibile, inodore, asonoro, im<hyphen
 				>-</hyphen
-			><br />
-			palpabile e insipido, ma anche razzista.
+			><br />palpabile e insipido, ma anche razzista.
 		</p>
 		<p>
-			È nonidemo studiata da apposite discipline e amministrata<br />
+			È non<choice><sic>idem</sic><corr>&nbsp;dimen</corr></choice>o studiata da apposite discipline
+			e amministrata<br />
 			da specifiche istituzioni quali filosofie e psicologie da una<br />
 			parte, chie<subst><delete>de</delete><add place="over">se</add></subst> e manicomi dall’altra,
 			per non citare che le più<br />
@@ -64,7 +65,7 @@
 				>, essendo questa
 				<!-- <movedBlockRef data-ref="1" /> -->
 				stata
-				<movedBlock data-ref="1" data-label="parola riposizionata">loro</movedBlock>
+				<movedBlock data-ref="." data-label="parola riposizionata">loro</movedBlock>
 				concessa soltanto<br />
 				nel per gentile concessione del Concilio
 			</delete><br />
